5e28f886aa87282de387af2a3d419f43658fd180
IvanKayukoff <ivankayukoff@gmail.com>
2019-04-16 19:33:01 +0300

    Add common domain classes

---
+++ backend/src/main/java/ru/ifmo/se/lab4/service/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package ru.ifmo.se.lab4.service;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import ru.ifmo.se.lab4.domain.User;
+import ru.ifmo.se.lab4.repository.UserRepository;
+
+@Component
+@NoArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user != null) {
+            return user;
+        } else {
+            throw new UsernameNotFoundException("User not found");
+        }
+    }
+
+}
