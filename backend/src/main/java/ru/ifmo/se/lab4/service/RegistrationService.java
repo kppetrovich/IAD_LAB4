672bb929e473050907e79df3ea5cc8dcd42b4e41
IvanKayukoff <ivankayukoff@gmail.com>
2019-04-17 01:25:31 +0300

    Take off registration messages

--- backend/src/main/java/ru/ifmo/se/lab4/service/RegistrationService.java
+++ backend/src/main/java/ru/ifmo/se/lab4/service/RegistrationService.java
@@ -3,6 +3,7 @@ package ru.ifmo.se.lab4.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import ru.ifmo.se.lab4.config.RegistrationMessage;
 import ru.ifmo.se.lab4.domain.User;
 import ru.ifmo.se.lab4.dto.RegistrationResponseDto;
 import ru.ifmo.se.lab4.repository.UserRepository;
@@ -22,21 +23,21 @@ public class RegistrationService {
 
     public RegistrationResponseDto registerUser(User user) {
         User received = userRepository.findByUsername(user.getUsername());
-        if (received != null) { // FIXME take off the messages
-            return new RegistrationResponseDto(false, "Username is already taken");
+        if (received != null) {
+            return new RegistrationResponseDto(false, RegistrationMessage.USERNAME_IS_TAKEN);
         }
 
         if (user.getUsername().length() < 3) {
-            return new RegistrationResponseDto(false, "Username's length can't be less than 3");
+            return new RegistrationResponseDto(false, RegistrationMessage.USERNAME_TOO_SHORT);
         }
         if (user.getPassword().length() < 3) {
-            return new RegistrationResponseDto(false, "Password's length can't be less than 3");
+            return new RegistrationResponseDto(false, RegistrationMessage.PASSWORD_TOO_SHORT);
         }
 
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
 
-        return new RegistrationResponseDto(true, "You've been successfully registered");
+        return new RegistrationResponseDto(true, RegistrationMessage.REGISTER_SUCCESS);
     }
 
 }
