2367de33169ff8449dad7842a9c1c11861fd70fb
IvanKayukoff <ivankayukoff@gmail.com>
2019-04-22 02:06:26 +0300

    Fix transaction error when calling clear-point

--- backend/src/main/java/ru/ifmo/se/lab4/repository/HistoryEntryRepository.java
+++ backend/src/main/java/ru/ifmo/se/lab4/repository/HistoryEntryRepository.java
@@ -1,6 +1,7 @@
 package ru.ifmo.se.lab4.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.ifmo.se.lab4.domain.HistoryEntry;
 import ru.ifmo.se.lab4.domain.User;
 
@@ -10,6 +11,7 @@ public interface HistoryEntryRepository extends JpaRepository<HistoryEntry, Long
 
     List<HistoryEntry> findAllByOwner(User owner);
 
+    @Transactional
     void removeAllByOwner(User owner);
 
 }
